45cc049bf93dc08527ec9e5816233d869d75ee0e
Hunyinbo Sewedo <82904055+techie1214@users.noreply.github.com>
2023-12-01 18:55:36 -0800

    Add files via upload

---
+++ src/template/indexPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import svg from "../assets/paynext.svg";
+// import './indexPage.css';
+
+const newStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    width: "100vw",
+    backgroundColor: "#34A8FA9E",
+    margin: "-40px 0px -20px -20px",
+}
+
+
+let Index = () => {
+  return (
+    <div style={newStyle} >
+      <img src= {svg} alt="PayNext"/>
+    </div>
+  );
+}
+
+export default Index;
